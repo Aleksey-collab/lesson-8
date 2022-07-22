496cff63039df51fa0c46b8915a359475faded7d
Aleksey <rgd666@yandex.ru>
2022-07-22 21:14:22 +0300

    example01

---
+++ example01/Program.cs
@@ -0,0 +1,31 @@
+﻿int [,] myArray = new int [3,4]
+{
+    {1,7,4,2},
+    {5,9,2,3},
+    {8,4,2,4}
+};
+int temp = 0;
+Console.WriteLine("Это наш массив ");
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        Console.Write(myArray[i,j] + ", ");
+    
+    }
+    
+    for (int k = 0; k < myArray.GetLength(1) -1; k++)
+    {
+        if (myArray[i, k] > myArray[i, k + 1]) 
+        {
+            temp = myArray[i, k];
+
+            myArray[i, k] = myArray[i, k + 1];
+
+            myArray[i, k + 1] = temp;
+        }
+        System.Console.WriteLine(myArray[i, k]);
+    }
+    Console.WriteLine();
+
+}
