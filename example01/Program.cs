52dbc806f354846eae74a50f68e733c248b0aca5
Aleksey <rgd666@yandex.ru>
2022-07-23 11:32:05 +0300

    example01

--- example01/Program.cs
+++ example01/Program.cs
@@ -1,31 +1,56 @@
-﻿int [,] myArray = new int [3,4]
+﻿int [,] myArray = new int [3, 4]
 {
     {1,7,4,2},
     {5,9,2,3},
     {8,4,2,4}
 };
-int temp = 0;
-Console.WriteLine("Это наш массив ");
+Console.WriteLine("Это мой массив ");
+//Console.Write(myArray);
 for (int i = 0; i < myArray.GetLength(0); i++)
 {
+    Console.Write("[ ");
     for (int j = 0; j < myArray.GetLength(1); j++)
     {
-        //Console.Write(myArray[i,j] + ", ");
-    
+        Console.Write(myArray[i, j]+ ", ");
     }
-    
-    for (int k = 0; k < myArray.GetLength(1) -1; k++)
-    {
-        if (myArray[i, k] > myArray[i, k + 1]) 
-        {
-            temp = myArray[i, k];
-
-            myArray[i, k] = myArray[i, k + 1];
-
-            myArray[i, k + 1] = temp;
-        }
-        
-    }
+    Console.Write("]");
     Console.WriteLine();
-
 }
+
+void SorTing(int[,] myArray)
+{
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < myArray.GetLength(1) - 1; z++) // цикл для сравнения и обмена
+            {
+                if (myArray[i, z] > myArray[i, z + 1]) //для изменения сортировки поменять знак > на противоположный
+                    {
+                    int temp = 0;
+                    temp = myArray[i, z];
+                    myArray[i, z] = myArray[i, z + 1];
+                    myArray[i, z + 1] = temp;
+                }
+            }
+        }
+    }
+}
+SorTing(myArray);
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+
+void PrintArray(int[,] myarray)
+{
+    for (int i = 0; i < myarray.GetLength(0); i++) // цикл по строкам.
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < myarray.GetLength(1); j++) // цикл по столбцам.
+        {
+             Console.Write(myarray[i,j] + ","); 
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+}
+PrintArray(myArray);
