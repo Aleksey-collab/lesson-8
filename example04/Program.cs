d363f97854049b5eddd8bdcf71b83cb94b2f0198
Aleksey <rgd666@yandex.ru>
2022-07-23 17:53:53 +0300

    example04

---
+++ example04/Program.cs
@@ -0,0 +1,72 @@
+﻿//Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, 
+//добавляя индексы каждого элемента.
+//массив размером 2 x 2 x 2
+//12(0,0,0) 22(0,0,1)
+//45(1,0,0) 53(1,0,1)
+
+
+int deep1 = InputInt("Введите размерность 1: ");
+int deep2 = InputInt("Введите размерность 2: ");
+int deep3 = InputInt("Введите размерность 3: ");
+int countNums = 89;
+
+if (deep1 * deep2 * deep3 > countNums)
+{
+    Console.Write("Массив слишком большой");
+    return;
+}
+
+int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
+
+for (int i = 0; i < resultNums.GetLength(0); i++)
+{
+    for (int j = 0; j < resultNums.GetLength(1); j++)
+    {
+        for (int k = 0; k < resultNums.GetLength(2); k++)
+        {
+            Console.WriteLine($"{resultNums[i, j, k]}({i},{j},{k})");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int[,,] Create3DMassive(int size1, int size2, int size3)
+{
+    int[,,] myArray = new int[size1, size2, size3];
+    int[] arr = new int[countNums];
+    int num = 10;
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = num++;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int randomInd = new Random().Next(0, arr.Length);
+        int temp = arr[i];
+        arr[i] = arr[randomInd];
+        arr[randomInd] = temp;
+    }
+
+    int arrIndex = 0;
+
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < myArray.GetLength(2); k++)
+            {
+                myArray[i, j, k] = arr[arrIndex++];
+            }
+        }
+    }
+    return myArray;
+}
+
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
